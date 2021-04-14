e057c2337ad678cb5153a2407044cad323ea6e06
keffren <matsergir1@alum.us.es>
2021-04-14 13:56:03 +0200

    impl express

---
+++ index.js
@@ -0,0 +1,25 @@
+//imports
+var express = require("express");
+var path = require("path");
+const { json } = require("body-parser");
+
+//Attributes
+const PORT = (process.env.PORT || 1607);
+
+
+
+//Start of the application
+var app = express();
+app.use(express.json());
+
+
+//Static navigation
+app.use("/", express.static(path.join(__dirname + "/public"))); 
+
+//Import API
+
+
+//Server running
+app.listen(PORT, () =>{
+    console.log("Server running at port:" + PORT);
+});
