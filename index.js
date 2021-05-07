ff15e187c7afd6de5c104191007b5bef563b7e7f
keffren <matsergir1@alum.us.es>
2021-05-07 10:41:53 +0200

    Carga DB automatizada al iniciar servidor

--- index.js
+++ index.js
@@ -7,27 +7,46 @@ var Datastore = require('nedb');
 //Attributes
 const PORT = (process.env.PORT || 1607);
 
+//Inicializar DB antes de arrancar app
+var razasDB = new Datastore();
+
+    let initRazasData = require("./clasesAPI/clases");
+    razasDB.insert(initRazasData, (err, dataAdded) => {
+        if(err){
+            console.error('--RazasAPI:\n  ERROR : the race hasn´t inserted into DataBase!');
+        }else{
+            console.log('--RazaAPI:\n  Race inserted into DataBase')
+        }
+    });
+
+var clasesDB = new Datastore();
+
+    let initClasesData = require("./clasesAPI/clases");
+    clasesDB.insert(initClasesData, (err, dataAdded) => {
+        if(err){
+            console.error('--ClasesAPI:\n  ERROR : the class hasn´t inserted into DataBase!');
+        }else{
+            console.log('--ClasesAPI:\n  Class inserted into DataBase')
+        }
+    });
+
 //Start of the application
 var app = express();
 app.use(express.json());
-
 
 //Static navigation
 app.use("/", express.static(path.join(__dirname + "/public"))); 
 
 //Import API: Razas
 var razasAPI = require('./razasAPI');
-var razasDB = new Datastore();
 razasAPI.loadDB(app,razasDB);
 razasAPI.httpCRUD(app,razasDB);
 
 //Import API: CLases
 var clasesAPI = require("./clasesAPI");
-var clasesDB = new Datastore();
+
 clasesAPI.loadDB(app,clasesDB);
 clasesAPI.httpCRUD(app,clasesDB);
-
-
 
 //Server running
 app.listen(PORT, () =>{
